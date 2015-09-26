f127b65db22852273f7effe5b0d2888b64c51333
kathrynfreesmeier <kathrynfreesmeier@yahoo.com>
2015-09-26 10:13:26 -0400

    Changed main.js
    Added phone detection for increased Rick Roll Time

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,14 @@
-setTimeout(function () {
-    window.location.href = "http://m.youtube.com/watch?v=dQw4w9WgXcQ";
-}, 2000);
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+	console.log("phonneeeesssssss")
+	setTimeout(function () {
+	    window.location.href = "http://m.youtube.com/watch?v=dQw4w9WgXcQ";
+	}, 6000);	
+} else {
+		setTimeout(function () {
+	    window.location.href = "http://m.youtube.com/watch?v=dQw4w9WgXcQ";
+	}, 3000);
+}
+
 
 particlesJS.load('particles-js', 'static/js/particlesjs-config.json', function() {
   console.log('callback - particles.js config loaded');
