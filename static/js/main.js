0323d823150da6413818ae394c341f49d7319c17
kathrynfreesmeier <kathrynfreesmeier@yahoo.com>
2015-09-26 09:55:33 -0400

    Changed index.html
    Added particles.js and implemented

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,7 @@
 setTimeout(function () {
     window.location.href = "http://m.youtube.com/watch?v=dQw4w9WgXcQ";
-}, 1000);
+}, 2000);
+
+particlesJS.load('particles-js', 'static/js/particlesjs-config.json', function() {
+  console.log('callback - particles.js config loaded');
+});
